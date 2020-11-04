b857c442eca1143da4643d3cf46fa75c5cc25598
Adri Torres <adriana.amaral@msn.com>
2020-11-04 17:11:31 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -33,7 +33,7 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   binding.pry
-  aoh[1][:name] = "Joelle van Dyne (PGOAT)"
+  aoh[1][:name] = "Libby Torres"
   
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
