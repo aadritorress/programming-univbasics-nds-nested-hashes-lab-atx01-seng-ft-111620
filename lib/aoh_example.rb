ed18700ec29d008e74f52c54c474ccdeab64ef5f
Adri Torres <adriana.amaral@msn.com>
2020-11-04 17:16:03 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -32,9 +32,8 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-directors[0] #=> {:name=>"Ida Lupino", :favorite_movie=>"The Hitch-Hiker"}
-directors[0][:name] #=> "Ida Lupino"
-directors[0][:name] = "John Ford"
+
+directors[1][:name] = "Libby Torres"
   aoh[1][:name] = 
   
   # Update the AoH data at row and key to have the value of new_value
